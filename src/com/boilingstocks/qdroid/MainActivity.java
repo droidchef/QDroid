bbcdcfc53b9e7dda25e25e7b2b76062741b75072
Ishan Khanna <boilingstocks@gmail.com>
2013-07-23 04:41:38 +0530

    Minute Changes

--- src/com/boilingstocks/qdroid/MainActivity.java
+++ src/com/boilingstocks/qdroid/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
